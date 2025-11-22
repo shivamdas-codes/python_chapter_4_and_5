e5434987f472cde1eb01c7ba48d98d18a15fb95a
shivam <shivam984861@gmail.com>
2025-11-22 19:43:18 +0530

    new changes

--- dictionary.py
+++ dictionary.py
@@ -1,13 +1,12 @@
 dict = {
-
-    "name" : "shivam das",
-    "age" : 21,
-    "marks" : 99.0,
-    "is_adult" : True,
-    "subjects" : ["python", "C", "java"],
-    "topics" : ("dictionary", "sets"),
- 
-}
+    "name" : "shivam das",                     # "KEY" is "STRING" and the "KEYVALUE" is "STRING"
+    "age" : 21,                                # "KEY" is "STRING" and the "KEYVALUE" is "INTEGER"
+    "marks" : 99.0,                            # "KEY" is "STRING" and the "KEYVALUE" is "FLOAT"
+    "is_adult" : True,                         # "KEY" is "STRING" and the "KEYVALUE" is "BOOLEAN"
+    "subjects" : ["python", "C", "java"],      # "KEY" is "STRING" and the "KEYVALUE" is "LIST"
+    "topics" : ("dictionary", "sets"),         # "KEY" is "STRING" and the "KEYVALUE" is "TUPLE"
+# in the dictionary the "keys" also can be written in the form of str(), int(), boolean() and tuple()
+    }
 print(type(dict))
 print(len(dict))
 print(dict)
