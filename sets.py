4fd52ea5781fdd50215218a7e1cd94fd01ec6a49
shivam <shivam984861@gmail.com>
2025-11-25 19:32:48 +0530

    new changes

--- sets.py
+++ sets.py
@@ -1,6 +1,6 @@
-set = {1,3,5}
-print(type(set)) # type of the set
-print(set)
+set0 = {1,3,5}
+print(type(set0)) # type of the set
+print(set0)
 # the above code represents the set.
 
 
@@ -13,5 +13,12 @@ set2 = {1,1,2,3,4,3, "hello", "world", "hello"}
 print(len(set2)) #the lenght also ignore the duplicate elements and print the unique elements
 # though we know that sets are unorder and each element in the set are unique, it doesn't print any duplicate and alwayes takes the first element. 
 print(set2)
+
+
+# empty set():
+empty_set = set()
+print(type(empty_set))
+print(empty_set)
+# the above code represents the empty set().
 
 
