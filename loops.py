c9bad08f3b843e0e364774991e950d166aa1ae34
shivam <shivam984861@gmail.com>
2025-11-29 22:41:51 +0530

    new changes

---
+++ loops.py
@@ -0,0 +1,10 @@
+# while True:
+#     print("hello")  
+"""in this case the loop never stop at any point because the givenh value is always 'true'
+if we want to stop loop at any point then the value should contain false at any point."""
+
+i = 1       # variable/iterator 'i' is assigned by 1
+while i <= 5:   #loop performed till <=5
+    print("hello")
+    i += 1      #every time the loop takes place it will increase by "1" till 5
+    print(i)    #this indicates how many times the loop has occured
